e512621f704357af3ce7819d89adcc091c1299eb
agriverae <ali.rivera.echegaray@gmail.com>
2019-06-05 13:06:39 -0300

    complete input component render

--- src/Input.js
+++ src/Input.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Input extends Component {
+
     render() {
-        return <div>
-            <button></button>
-        </div>
+        const contents = this.props.success
+        ? null
+        : (
+            <form classname="form-inline">
+                <input 
+                    className="mb-2 mx-sm-3"
+                    data-test="input-box"
+                    id="word-guess"
+                    type="text"
+                    placeholder="Enter guess" />
+                <button 
+                    className="btn btn-primary mb-2"
+                    data-test="submit-button"
+                    type="submit">
+                    Submit
+                </button>
+            </form>
+        )
+
+        return (
+            <div data-test="component-input">
+                {contents}
+            </div>
+        )
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({ success }) => {
     return {};
 }
 
